c17a1c17989907263dfbabeaaa3fb63459f16019
mh-cbon <mh-cbon@users.noreply.github.com>
2016-03-05 12:54:09 +0100

    Add multicast-dns options

--- README.md
+++ README.md
@@ -23,16 +23,25 @@ ctrl+c to quit
 
     Options
 
-      --host | -H     Host which your server listens
-      --port | -P     Port which your server listens
-      --type | -T     Type of interface provided by your server
-      --verbose | -v  Enable verbosity pass in the module list to debug.
+      --host | -H       Host which your server listens
+      --port | -P       Port which your server listens
+      --type | -T       Type of interface provided by your server
+      --multicast | -M  Use UDP multicasting
+      --interface | -I  Explicitly specify a network interface. Defaults to all.
+      --udpport         Set the UDP port
+      --udpip           Set the UDP IP
+      --ttl | -T        Set the multicast TTL
+      --loopback | -L   Receive your own packets
+      --reuseAddr | -R  Type of interface provided by your server
+      --verbose | -v    Set the reuseAddr on the socket (node >=0.11.13)
 
     Examples
-      bonjour-publish -H 127.0.0.1 -P 8088 -T rc
-      bonjour-publish -H 127.0.0.1 -P 8088 -T http
+      bonjour-publish -H 127.0.0.1 -P 8088 -T rc "hello!"
+      bonjour-publish -H 127.0.0.1 -P 8088 -T http "I love you,"
+      bonjour-publish -L -H 127.0.0.1 -P 8088 -T http "Could you give me your name ?"
 
 # read more
 
 - https://github.com/watson/bonjour
+- https://github.com/mafintosh/multicast-dns
 - https://github.com/watson/bonjour-browser
--- index.js
+++ index.js
@@ -8,24 +8,42 @@ Usage
 
 Options
 
-  --host | -H     Host which your server listens
-  --port | -P     Port which your server listens
-  --type | -T     Type of interface provided by your server
-  --verbose | -v  Enable verbosity pass in the module list to debug.
+  --host | -H       Host which your server listens
+  --port | -P       Port which your server listens
+  --type | -T       Type of interface provided by your server
+  --multicast | -M  Use udp multicasting
+  --interface | -I  Explicitly specify a network interface. Defaults to all.
+  --udpport | -U    Set the udp port
+  --ip              Set the udp IP
+  --ttl | -T        Set the multicast TTL
+  --loopback | -L   Receive your own packets
+  --reuseAddr | -R  Type of interface provided by your server
+  --verbose | -v    Set the reuseAddr on the socket (node >=0.11.13)
 
 Examples
   bonjour-publish -H 127.0.0.1 -P 8088 -T rc
   bonjour-publish -H 127.0.0.1 -P 8088 -T http
+
 */}
 var pkg   = require('./package.json')
 var argv  = require('minimist')(process.argv.slice(2));
 var help  = require('@maboiteaspam/show-help')(usage, argv.h||argv.help, pkg)
 var debug = require('@maboiteaspam/set-verbosity')(pkg.name, argv.v || argv.verbose);
 
-const host = argv.host || argv.H || false;
-const port = argv.port || argv.P || false;
-const type = argv.type || argv.T || false;
-const title = argv['_'].length && argv['_'][0] || "nop, no title set";
+// bonjour options
+const host      = argv.host || argv.H || false;
+const port      = argv.port || argv.P || false;
+const type      = argv.type || argv.T || false;
+const title     = argv['_'].length && argv['_'][0] || "nop, no title set";
+
+// mdns options
+const mcast     = argv.multicast || argv.M || null;
+const iface     = argv.interface || argv.I || null;
+const uPort     = argv.udpport || null;
+const uIP       = argv.udpip || null;
+const TTL       = argv.ttl || argv.T || null;
+const loopback  = argv.loopback || argv.L || null;
+const reuseAddr = argv.reuseAddr || argv.R || null;
 
 (!port && !type) && help.print(usage, pkg) && help.die(
   "Wrong invokation"
@@ -33,7 +51,16 @@ const title = argv['_'].length && argv['_'][0] || "nop, no title set";
 
 console.log("Announce '%s' on %s:%s as %s", title, host, port, type)
 
-var bonjour = require('bonjour')()
+var mdnsOpts = {};
+if (mcast)      mdnsOpts.multicast  = mcast;
+if (iface)      mdnsOpts.interface  = iface;
+if (uPort)      mdnsOpts.port       = uPort;
+if (uIP)        mdnsOpts.ip         = uIP;
+if (TTL)        mdnsOpts.ttl        = TTL;
+if (loopback)   mdnsOpts.loopback   = loopback;
+if (reuseAddr)  mdnsOpts.reuseAddr  = reuseAddr;
+
+var bonjour = require('bonjour')(mdnsOpts);
 
 // advertise your server
 var opts = { name: title, type: type, port: port, host: host };
